dc1f3f041705715aa41b98d7c6a390ccab9925ed
qwavy <nursultanr585@gmail.com>
2024-06-15 18:20:04 +0500

    add:footer

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,20 +1,23 @@
 <template>
-  <div>
-    <span style="margin-right: 50px">count</span>
-    <span>{{ count }}</span>
-  </div>
-  <div>
-    <span style="margin-right: 50px">dubleCount</span>
-    <span>{{ dubleCount }}</span>
-  </div>
-  <button @click="increment">increment</button>
-  <button @click="clearUser">clear</button>
+<!--  <div>-->
+<!--    <span style="margin-right: 50px">count</span>-->
+<!--    <span>{{ count }}</span>-->
+<!--  </div>-->
+<!--  <div>-->
+<!--    <span style="margin-right: 50px">dubleCount</span>-->
+<!--    <span>{{ dubleCount }}</span>-->
+<!--  </div>-->
+<!--  <button @click="increment">increment</button>-->
+<!--  <button @click="clearUser">clear</button>-->
+  <Footer/>
 </template>
 
 <script lang="ts">
 import { mapActions, mapState } from "pinia";
 import { useCounterStore } from "@/store/counter";
+import Footer from "@/components/UI/Footer.vue";
 export default {
+  components: {Footer},
   computed: {
     ...mapState(useCounterStore, ["count", "dubleCount"]),
   },
