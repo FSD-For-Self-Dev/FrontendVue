a727fd1afb9ad3c205e5a084e60a49f424485748
Buzina Marina <77714654+BU-Marina@users.noreply.github.com>
2024-09-11 10:47:33 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 node -v
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 sudo nvm install 22.4.1
 sudo nvm use 22.4.1
 node -v
