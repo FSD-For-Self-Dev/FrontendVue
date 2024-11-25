d32cf5081f588396f88db4322171ae5c702ab062
huggerkios <hugger.kiosk0r@icloud.com>
2024-11-25 16:25:14 +0300

    refactoring/add "global" folder to keep all of global components; add reusable component for SVG icons

---
+++ src/components/global/svg-icon/constants.ts
@@ -0,0 +1,25 @@
+export const sizes = {
+    sm: {
+      size: 16,
+      strokeWidth: 1
+    },
+    md: {
+      size: 24,
+      strokeWidth: 1
+    },
+    lg: {
+      size: 32,
+      strokeWidth: 1
+    },
+    xl: {
+      size: 44,
+      strokeWidth: 1.5
+    }
+};
+
+export const defaults = {
+    color: 'var:neutrals-900',
+    varPrefix: 'q-',
+    hoverColor: 'var:primary-500',
+    size: 'md'
+};
