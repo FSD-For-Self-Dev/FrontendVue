d4137425c57f7b103905f0055404588f98cf3582
huggerkios <hugger.kiosk0r@icloud.com>
2024-12-02 19:58:25 +0300

    fix/styles fix

--- src/components/UI/modal/modal-content/AddLanguagesForm.vue
+++ src/components/UI/modal/modal-content/AddLanguagesForm.vue
@@ -15,14 +15,14 @@ export default {
   props: {
     closeForm: {
       type: Function,
-      required: true
+      required: true,
     },
   },
   data() {
     return {
       searchLanguage: '',
-      activeLanguage: [] as LanguageDto[]
-    }
+      activeLanguage: [] as LanguageDto[],
+    };
   },
   computed: {
     ...mapState(useLanguagesStore, ['available_languages']),
@@ -30,18 +30,24 @@ export default {
       return this.filteredLanguages.length;
     },
     available_info() {
-      return numWord(this.count, ['Доступен ', 'Доступно ', 'Доступно ']) + this.count + numWord(this.count, [' язык', ' языка', ' языков']);
+      return (
+        numWord(this.count, ['Доступен ', 'Доступно ', 'Доступно ']) +
+        this.count +
+        numWord(this.count, [' язык', ' языка', ' языков'])
+      );
     },
     filteredLanguages() {
       return this.available_languages.filter((lang) => {
-        return lang.name.toLowerCase().includes(this.searchLanguage.toLowerCase()) ||
+        return (
+          lang.name.toLowerCase().includes(this.searchLanguage.toLowerCase()) ||
           lang.name_local.toLowerCase().includes(this.searchLanguage.toLowerCase())
+        );
       });
-    }
+    },
   },
   methods: {
-    ...mapActions(useLanguagesStore, ["postLearningLanguage", "getAvailableLanguages"]),
-    ...mapActions(useNotificationsStore, ["addNewMessage"]),
+    ...mapActions(useLanguagesStore, ['postLearningLanguage', 'getAvailableLanguages']),
+    ...mapActions(useNotificationsStore, ['addNewMessage']),
     toggleActiveLanguage(lang: LanguageDto) {
       if (this.activeLanguage.includes(lang)) {
         this.activeLanguage = this.activeLanguage.filter((item) => item.id !== lang.id);
@@ -56,16 +62,22 @@ export default {
       const res = await this.postLearningLanguage(this.activeLanguage);
       if (isAxiosError(res)) {
         if (res.response?.status === 409) {
-          this.addNewMessage({ type: 'error', text: 'Максимальное количество изучаемых языков' });
+          this.addNewMessage({
+            type: 'error',
+            text: 'Максимальное количество изучаемых языков',
+          });
         }
 
         return;
       }
       const lenWords = this.activeLanguage.length;
-      const addWord = numWord(lenWords, ['Добавлен', 'Добавлено', 'Добавлено'])
+      const addWord = numWord(lenWords, ['Добавлен', 'Добавлено', 'Добавлено']);
       const learnWord = numWord(lenWords, ['изучаемый', 'изучаемых', 'изучаемых']);
       const langWord = numWord(lenWords, ['язык', 'языка', 'языков']);
-      this.addNewMessage({ type: 'info', text: `${addWord} ${lenWords} ${learnWord} ${langWord}` })
+      this.addNewMessage({
+        type: 'info',
+        text: `${addWord} ${lenWords} ${learnWord} ${langWord}`,
+      });
 
       await this.getAvailableLanguages();
       this.closeForm();
@@ -75,8 +87,8 @@ export default {
       event.preventDefault();
       container.scrollTop += event.deltaX;
     },
-  }
-}
+  },
+};
 </script>
 
 <template>
@@ -84,17 +96,30 @@ export default {
     <Search v-model="searchLanguage" />
     <div class="languages-from--counters-info">
       <span class="languages-form--available-info">{{ available_info }} </span>
-      <Counter :value="activeLanguage.length" :clear-active-items="clearActiveLanguages" />
+      <Counter
+        :value="activeLanguage.length"
+        :clear-active-items="clearActiveLanguages"
+      />
     </div>
     <div class="languages-form--list" @wheel.prevent="handleWheel">
-      <div class="languages-form--list-item" @click="() => toggleActiveLanguage(lang)"
-        :class="activeLanguage.includes(lang) && 'active'" v-for="lang in filteredLanguages" :key="lang.id">
+      <div
+        class="languages-form--list-item"
+        @click="() => toggleActiveLanguage(lang)"
+        :class="activeLanguage.includes(lang) && 'active'"
+        v-for="lang in filteredLanguages"
+        :key="lang.id"
+      >
         <img :src="lang.flag_icon" />
         <span>{{ lang.name_local }}</span>
       </div>
     </div>
     <div class="languages-form--actions">
-      <Button text="Отменить" variant="secondary" size="medium" @click="() => closeForm()" />
+      <Button
+        text="Отменить"
+        variant="secondary"
+        size="medium"
+        @click="() => closeForm()"
+      />
       <Button text="Добавить" variant="primary" size="medium" type="submit" />
     </div>
   </form>
@@ -151,8 +176,8 @@ export default {
       }
 
       &.active {
-        background-color: $primary-200;
-        border: 1px solid $primary-500;
+        background-color: $primary-300;
+        border: 1px solid $primary-300;
       }
     }
   }
