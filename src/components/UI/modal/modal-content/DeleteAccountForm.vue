952b5389fda11a3275a54746dcf9641dce547b51
huggerkios <hugger.kiosk0r@icloud.com>
2024-12-04 17:11:07 +0300

    fix/update DeleteAccountForm style

--- src/components/UI/modal/modal-content/DeleteAccountForm.vue
+++ src/components/UI/modal/modal-content/DeleteAccountForm.vue
@@ -3,7 +3,7 @@ import { useUserStore } from '@/store/user';
 import { useNotificationsStore } from '@/store/notifications';
 import { useVocabularyStore } from '@/store/vocabulary';
 import { useLanguagesStore } from '@/store/languages';
-import { mapActions } from 'pinia';
+import { mapActions, mapState } from 'pinia';
 import Button from '@/components/UI/button/Button.vue';
 
 export default {
@@ -13,6 +13,10 @@ export default {
       type: Function,
       required: true,
     },
+  },
+  computed: {
+    ...mapState(useUserStore, ['username', 'first_name', 'image', 'native_languages']),
+    ...mapState(useLanguagesStore, ['learning_languages', 'global_languages']),
   },
   methods: {
     ...mapActions(useUserStore, ['deleteUser']),
@@ -30,27 +34,120 @@ export default {
       this.closeForm();
       this.$router.push('/');
     },
+    getFlagIcon(neededLang: string | undefined) {
+      return this.global_languages.find((lang) => lang.name === neededLang)?.flag_icon;
+    },
   },
 };
 </script>
 
 <template>
   <form @submit.prevent="handleDelete" class="delete-account-form">
-    <Button
-      type="button"
-      variant="secondary"
-      @click="() => closeForm()"
-      text="Отменить"
-      size="medium"
-    />
-    <Button type="submit" variant="danger" text="Подтвердить" size="medium" />
+    <div class="user-info">
+      <img :src="image" class="avatar" />
+      <div class="info">
+        <div class="name">
+          <p id="first-name">{{ first_name }}</p>
+          <p id="username">@{{ username }}</p>
+        </div>
+        <div class="languages">
+          <p class="small-title">Родные языки:</p>
+          <div class="languages--item" v-for="language in native_languages">
+            <img :src="getFlagIcon(language)" alt="Icon" class="language-icon" />
+            <p>{{ language }}</p>
+          </div>
+        </div>
+        <div class="languages">
+          <p class="small-title">Словарь:</p>
+          <div class="languages--item" v-for="language in learning_languages">
+            <img :src="getFlagIcon(language.language.name)" alt="Icon" class="language-icon" />
+            <p>{{ language.words_count }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="buttons">
+      <Button
+        type="button"
+        variant="secondary"
+        @click="() => closeForm()"
+        text="Отменить"
+        size="medium"
+      />
+      <Button type="submit" variant="danger" text="Подтвердить" size="medium" />
+    </div>
   </form>
 </template>
 
 <style lang="scss">
 .delete-account-form {
   display: flex;
-  gap: 1.6rem;
-  justify-content: flex-end;
+  flex-direction: column;
+  gap: 4rem;
+
+  .user-info {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 4rem;
+
+    .avatar {
+      width: 20rem;
+      min-width: 20rem;
+      height: 20rem;
+      min-height: 20rem;
+      border-radius: $radius-full;
+    }
+
+    .info {
+      display: flex;
+      flex-direction: column;
+      gap: 2.4rem;
+      width: 100%;
+
+      .name {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 1.2rem;
+
+        #first-name {
+          @include subheading-2;
+        }
+
+        #username {
+          @include text-1;
+          color: $neutrals-600;
+        }
+      }
+
+      .languages {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 1.6rem;
+        @include text-1;
+        
+        &--item {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          gap: 0.8rem;
+          @include text-1;
+
+          .language-icon {
+            width: 2.4rem;
+            height: 2.4rem;
+          }
+        }
+      }
+    }
+  }
+
+  .buttons {
+    display: flex;
+    gap: 1.6rem;
+    justify-content: flex-end;
+  }
 }
 </style>
