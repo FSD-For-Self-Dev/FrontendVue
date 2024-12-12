7d2c10afec0e8046a34e54490ebacab94c7045e8
HanmaDaiki <daikihanma@yandex.ru>
2024-12-12 22:29:06 +0300

    add slider

---
+++ src/components/landing/slides/SlideOne.vue
@@ -0,0 +1,77 @@
+<script lang="ts">
+import WordCard from '@/components/vocabulary/WordCard.vue';
+import Img1 from '@/assets/images/adpage-image1.png';
+import Img2 from '@/assets/images/adpage-image2.png';
+import Img3 from '@/assets/images/adpage-image3.png';
+import { Slide } from 'vue3-carousel';
+import type { WordDto } from '@/dto/vocabulary.dto';
+
+export default {
+  components: { Slide, WordCard },
+  setup() {
+    const word: WordDto = {
+      activity_progress: 0,
+      activity_status: 'Неактивное',
+      author: 'user',
+      created: '2024-12-06 16:04',
+      favorite: true,
+      id: '8ed43223-58dc-488d-b0c3-2d9fa12zzz23',
+      image: Img1,
+      is_problematic: false,
+      language: 'Italian',
+      modified: '2024-12-06 16:04',
+      slug: 'spiaggia-user',
+      tags: ['Отпуск', 'А1'],
+      text: 'Spiaggia',
+      translations: ['Пляж'],
+      translations_count: 1,
+      types: [],
+    };
+    return {
+      word,
+      Img2,
+      Img3,
+    };
+  },
+};
+</script>
+
+<template>
+  <Slide :slide-size="{width: '344px', height: '545px'}"
+    ><div :class="$style.wrapper">
+      <WordCard :style="{ width: '100%' }" :word="word" />
+      <div :class="$style.sub_images">
+        <img :class="$style.image" :src="Img2" />
+        <img :class="$style.image" :src="Img3" />
+      </div>
+    </div>
+    <div></div
+  ></Slide>
+</template>
+
+<style lang="scss" module>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+  width: 34.5rem;
+  height: 54.5rem;
+}
+
+.word_card {
+  width: 100%;
+  height: 100%;
+}
+
+.sub_images {
+  display: flex;
+  gap: 0.7rem;
+  flex-direction: row;
+}
+
+.image {
+  width: 16.9rem;
+  height: 16.9rem;
+  border-radius: 2rem;
+}
+</style>
---
+++ src/components/landing/slides/SlideTwo.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { Slide } from 'vue3-carousel';
+export default {
+  components: { Slide },
+};
+</script>
+
+<template>
+  <Slide :slide-size="{width: '344px', height: '545px'}"> <div :class="$style.wrapper"></div></Slide>
+</template>
+
+<style lang="scss" module>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+  width: 34.5rem;
+  height: 54.5rem;
+  background-image: url('../../../assets/images/adpage-image5.png');
+}
+</style>
